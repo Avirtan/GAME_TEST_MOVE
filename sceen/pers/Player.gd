c31b6d4394b412e06640a810a2364f9864313027
Avirtan <36964697+Avirtan@users.noreply.github.com>
2019-03-17 18:25:26 +0300

    12

--- sceen/pers/Player.gd
+++ sceen/pers/Player.gd
@@ -147,7 +147,7 @@ func get_input(delta):
 			#$Anim.animation = "Run_R"
 			if !stena and time == -1:
 				velocity.x +=run_speed
-			if stena and time >1:
+			if stena and time >0.5:
 				velocity.x +=run_speed
 		elif hit and !is_on_floor() :
 			velocity.x +=run_speed*2
