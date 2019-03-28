c78c717dd822753efeff2511b20715c3edd42d62
Avirtan <36964697+Avirtan@users.noreply.github.com>
2019-03-28 16:40:04 +0300

    123

--- sceen/enym/vahtersha/vahtersha.gd
+++ sceen/enym/vahtersha/vahtersha.gd
@@ -4,26 +4,36 @@ var moveR = false
 var distance = Vector2()
 var velocity = Vector2()
 var direction = Vector2()
+var P = Vector2()
+var V = Vector2()
 export (bool) var directionR = false
 var speed = 80
 var gravity = 400
 var runs = false
 
-func checkDirect():
+func move():
 	if directionR:
 		direction.x = 1
 	else:
 		direction.x = -1
+	P = $"../Player".global_position
+	V = global_position
+	if V.x - P.x < 0:
+		direction.x = 1
+	elif V.x - P.x > 0:
+		direction.x = -1
+
+func checkDirect():
 	if velocity.x > 0:
 		moveR = true
 	elif velocity.x < 0:
 		moveR = false
 	if velocity.length()>0:
 		if moveR and runs:
-			speed =100
+			speed =200
 			$Anim.animation = "run_r"
 		elif !moveR and runs:
-			speed =100
+			speed =200
 			$Anim.animation = "run_l" 
 		else:
 			speed =50
@@ -36,10 +46,10 @@ func checkDirect():
 			pass
 			#$Anim.animation = "stopl" 
 func _ready():
-	runs = !false
+	runs = false
 
 func _physics_process(delta):
-	print(directionR)
+	move()
 	checkDirect()
 	distance.x = speed*delta
 	velocity.x = (direction.x*distance.x)/delta
